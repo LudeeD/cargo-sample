826dac12292057a9e14adb64049df7958731d69b
Luís Silva <luiscomsnofim@gmail.com>
2024-11-21 22:23:27 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use argh::FromArgs;
+use cargo_metadata::Metadata;
+use cargo_metadata::MetadataCommand;
+use cargo_metadata::Package;
+use crates_io_api::SyncClient;
 use inquire::Confirm;
 use inquire::Select;
 use std::{fs, path::PathBuf, process::Command};
@@ -21,23 +25,85 @@ struct Sample {
 }
 
 fn main() -> Result<()> {
-    let demo = argh::from_env::<Sample>();
+    let args = argh::from_env::<Sample>();
+
+    let home_dir = match args.output {
+        Some(output) => PathBuf::from(output),
+        None => PathBuf::from("."),
+    };
+
+    if !home_dir.exists() {
+        fs::create_dir_all(&home_dir)?;
+    }
+
+    std::env::set_current_dir(&home_dir)?;
+
+    println!("Current directory: {:?}", std::env::current_dir()?);
+
+    // add the dependency we want to the project
+
+    let cargo_add = Command::new("cargo").arg("add").arg(&args.repo).status()?;
+
+    if cargo_add.success() {
+        println!("Added dependency: {}", args.repo);
+    } else {
+        println!("Probably not a Cargo project... creating a new one and adding the dependency");
+        Command::new("cargo").arg("init").status()?;
+        Command::new("cargo").arg("add").arg(&args.repo).status()?;
+    }
+
+    let metadata = MetadataCommand::new().exec()?;
+
+    // filter the dependency we want
+    let deps: Vec<&Package> = metadata
+        .packages
+        .iter()
+        .filter(|package| package.name == args.repo)
+        .collect();
+
+    assert!(deps.len() == 1, "Something is cooked");
+
+    let package = deps[0];
+    let manifest_path = PathBuf::from(package.manifest_path.clone());
+    let manifest_dir = manifest_path
+        .parent()
+        .with_context(|| anyhow!("Failed to get manifest dir"))?;
+    let vcs_info = manifest_dir.join(".cargo_vcs_info.json");
+
+    let mut repository = package.repository.to_owned().with_context(|| {
+        anyhow!(
+            "Failed to get repository from package: {:#?}",
+            package.repository
+        )
+    })?;
+    repository.push_str(".git");
+
+    // read json file
+    let info = fs::read_to_string(&vcs_info)?;
+    let vcs_info: serde_json::Value = serde_json::from_str(&info)?;
+    let sha1 = vcs_info["git"]["sha1"].as_str().unwrap();
+
+    println!("Git repository {:#?}", repository);
+    println!("Git repository {:#?}", sha1);
 
     // check if git is installed
     if Command::new("git").arg("--version").status().is_err() {
         return Err(anyhow::anyhow!("Git is not installed"));
     }
 
-    let repo = demo.repo;
-
     let temp_dir = tempfile::tempdir()?;
 
-    println!("Cloning repository: {}", repo);
+    println!("Cloning repository: {}", repository);
 
     Command::new("git")
-        .args(["clone", &repo, temp_dir.path().to_str().unwrap()])
+        .args(["clone", &repository, temp_dir.path().to_str().unwrap()])
         .output()
         .context("Failed to clone repository")?;
+
+    Command::new("git")
+        .args(["checkout", &sha1])
+        .output()
+        .context("Failed to checkout proper sha1")?;
 
     // Find the examples directory
     let examples_dir = temp_dir.path().join("examples");
@@ -61,13 +127,7 @@ fn main() -> Result<()> {
 
     let path = examples_dir.join(&ans);
 
-    // output is either the output folder or the current folder
-    let output = match demo.output {
-        Some(output) => PathBuf::from(output),
-        None => PathBuf::from("."),
-    };
-
-    let output_string = output.to_str().unwrap_or_default();
+    let output_string = home_dir.to_str().unwrap_or_default();
     let confirmation_string = format!("Copy example to {}", output_string);
     let ans = Confirm::new(&confirmation_string)
         .with_default(false)
@@ -77,7 +137,7 @@ fn main() -> Result<()> {
     match ans {
         Ok(true) => {
             println!("Copying example to {}", output_string);
-            copy_dir_recursively(&path, &output)?;
+            copy_dir_recursively(&path, &home_dir)?;
         }
         Ok(false) => {
             println!("That's too bad...");
